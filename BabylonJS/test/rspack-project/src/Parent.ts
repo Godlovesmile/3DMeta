d7fab5dae9611286da47e818db7d827cb6d6952c
lizhi <15501251520@163.com>
2023-03-20 19:02:12 +0800

    feat: Parent create line

--- BabylonJS/test/rspack-project/src/Parent.ts
+++ BabylonJS/test/rspack-project/src/Parent.ts
@@ -7,6 +7,8 @@ import {
   SceneLoader,
   Vector3,
   WebGPUEngine,
+  Mesh,
+  Color3,
 } from 'babylonjs'
 
 export default class Parent {
@@ -48,6 +50,22 @@ export default class Parent {
 
     new HemisphericLight('light', new Vector3(0, 1, 0), scene)
 
+    this.showAxis(6, scene)
     return scene
+  }
+
+  // === create and draw axes ===
+  showAxis(size, scene) {
+    const axisX = MeshBuilder.CreateLines('axisX', {
+      points: [
+        Vector3.Zero(),
+        new Vector3(size, 0, 0),
+        new Vector3(size * 0.95, 0.05 * size, 0),
+        new Vector3(size, 0, 0),
+        new Vector3(size * 0.95, -0.05 * size, 0),
+      ],
+    })
+
+    axisX.color = new Color3(1, 0, 0)
   }
 }
