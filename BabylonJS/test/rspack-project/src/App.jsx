d5c3f360e424011697b1dc101a7c5eb96399b3ec
lizhi <15501251520@163.com>
2023-03-20 17:25:57 +0800

    feat: Baylong.js Parent && Child

--- BabylonJS/test/rspack-project/src/App.jsx
+++ BabylonJS/test/rspack-project/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import React from 'react'
-import Village from './Village'
+import Parent from './Parent'
+// import Village from './Village'
 // import BasicScene from './BasicScene'
+
 
 class App extends React.Component {
   componentDidMount() {
     const renderCanvas = document.getElementById('renderCanvas')
 
     // new BasicScene(renderCanvas)
-    new Village(renderCanvas)
+    // new Village(renderCanvas)
+    new Parent(renderCanvas)
   }
 
   render() {
---
+++ BabylonJS/test/rspack-project/src/Parent.ts
@@ -0,0 +1,53 @@
+import {
+  ArcRotateCamera,
+  Engine,
+  HemisphericLight,
+  MeshBuilder,
+  Scene,
+  SceneLoader,
+  Vector3,
+  WebGPUEngine,
+} from 'babylonjs'
+
+export default class Parent {
+  engine: Engine
+  scene: Scene
+
+  constructor(private renderCanvas: HTMLCanvasElement) {
+    this._init()
+  }
+
+  async _init() {
+    const webGPUSupported = await (WebGPUEngine as any).IsSupportedAsync
+
+    if (webGPUSupported) {
+      this.engine = new WebGPUEngine(this.renderCanvas)
+
+      return
+    }
+
+    this.engine = new Engine(this.renderCanvas)
+    this.scene = this.createScene()
+
+    // register a render loop to repeatedly render the scene
+    this.engine.runRenderLoop(() => {
+      this.scene.render()
+    })
+
+    // watch fro browser/canvas resize events
+    window.addEventListener('resize', () => {
+      this.engine.resize()
+    })
+  }
+
+  createScene() {
+    const scene = new Scene(this.engine)
+    const camera = new ArcRotateCamera('camera', -Math.PI / 2.2, Math.PI / 2.5, 15, new Vector3(0, 0, 0))
+
+    camera.attachControl(this.renderCanvas, true)
+
+    new HemisphericLight('light', new Vector3(0, 1, 0), scene)
+
+    return scene
+  }
+}
