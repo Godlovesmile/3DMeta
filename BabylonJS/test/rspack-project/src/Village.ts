0921d48cc3bd30d31cb6724f5a79c0d857319b97
lizhi <15501251520@163.com>
2023-03-20 16:45:04 +0800

    feat: buildDwellings

--- BabylonJS/test/rspack-project/src/Village.ts
+++ BabylonJS/test/rspack-project/src/Village.ts
@@ -15,6 +15,7 @@ import {
   Vector4,
   WebGPUEngine,
 } from 'babylonjs'
+import 'babylonjs-loaders'
 
 export default class Village {
   engine: Engine
@@ -57,14 +58,63 @@ export default class Village {
 
     // const music = new Sound('cello', 'https://playground.babylonjs.com/sounds/cellolong.wav', scene, null, { loop: true, autoplay: true })
 
-    this.buildHouse(2)
-    this.buildGround()
+    // 建房子, 通过局部模式进行创建, roof + house + ground
+    // this.buildDwellings()
+
+    // 建房子, 通过整体模型进行创建
+    SceneLoader.ImportMeshAsync('', 'https://assets.babylonjs.com/meshes/', 'village.glb', scene)
 
     return scene
   }
-  buildRoof() {
+  buildDwellings() {
+    this.buildGround()
+
+    const detached_house = this.buildHouse(1)!
+    detached_house.rotation.y = -Math.PI / 16
+    detached_house.position.x = -6.8
+    detached_house.position.z = 2.5
+
+    const semi_house = this.buildHouse(2)!
+    semi_house.rotation.y = -Math.PI / 16
+    semi_house.position.x = -4.5
+    semi_house.position.z = 3
+
+    const places = [] //each entry is an array [house type, rotation, x, z]
+    places.push([1, -Math.PI / 16, -6.8, 2.5])
+    places.push([2, -Math.PI / 16, -4.5, 3])
+    places.push([2, -Math.PI / 16, -1.5, 4])
+    places.push([2, -Math.PI / 3, 1.5, 6])
+    places.push([2, (15 * Math.PI) / 16, -6.4, -1.5])
+    places.push([1, (15 * Math.PI) / 16, -4.1, -1])
+    places.push([2, (15 * Math.PI) / 16, -2.1, -0.5])
+    places.push([1, (5 * Math.PI) / 4, 0, -1])
+    places.push([1, Math.PI + Math.PI / 2.5, 0.5, -3])
+    places.push([2, Math.PI + Math.PI / 2.1, 0.75, -5])
+    places.push([1, Math.PI + Math.PI / 2.25, 0.75, -7])
+    places.push([2, Math.PI / 1.9, 4.75, -1])
+    places.push([1, Math.PI / 1.95, 4.5, -3])
+    places.push([2, Math.PI / 1.9, 4.75, -5])
+    places.push([1, Math.PI / 1.9, 4.75, -7])
+    places.push([2, -Math.PI / 3, 5.25, 2])
+    places.push([1, -Math.PI / 3, 6, 4])
+
+    //Create instances from the first two that were built
+    const houses = []
+    for (let i = 0; i < places.length; i++) {
+      if (places[i][0] === 1) {
+        houses[i] = detached_house.createInstance('house' + i)
+      } else {
+        houses[i] = semi_house.createInstance('house' + i)
+      }
+      houses[i].rotation.y = places[i][1]
+      houses[i].position.x = places[i][2]
+      houses[i].position.z = places[i][3]
+    }
+  }
+  buildRoof(width) {
     const roof = MeshBuilder.CreateCylinder('roof', { diameter: 1.3, height: 1.2, tessellation: 3 })
     roof.scaling.x = 0.75
+    roof.scaling.y = width
     roof.rotation.z = Math.PI / 2
     roof.position.y = 1.22
 
@@ -111,7 +161,7 @@ export default class Village {
     return box
   }
   buildHouse(width) {
-    const roof = this.buildRoof()
+    const roof = this.buildRoof(width)
     const box = this.buildBox(width)
     // 合并整体, 方便后续拿到整个 house 对象进行处理
     const house = Mesh.MergeMeshes([box, roof], true, false, undefined, false, true)
@@ -119,7 +169,7 @@ export default class Village {
     return house
   }
   buildGround() {
-    const ground = MeshBuilder.CreateGround('ground', { width: 10, height: 10 })
+    const ground = MeshBuilder.CreateGround('ground', { width: 15, height: 16 })
     const groundMat = new StandardMaterial('groundMat')
 
     groundMat.diffuseColor = new Color3(0, 1, 0)
