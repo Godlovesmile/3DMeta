15ced3e0f641e5a6c2df0665fe18bf879aa56bf2
lizhi <15501251520@163.com>
2023-04-04 15:06:31 +0800

    feat: car && village animation

--- BabylonJS/test/rspack-project/src/VillageAnimation.ts
+++ BabylonJS/test/rspack-project/src/VillageAnimation.ts
@@ -17,8 +17,7 @@ import {
   Vector4,
   Animation,
 } from 'babylonjs'
-// import * as earcut from 'earcut'
-// ;(window as any).earcut = earcut
+import 'babylonjs-loaders'
 
 export default class VillageAnimation {
   engine: Engine
@@ -53,14 +52,71 @@ export default class VillageAnimation {
 
   createScene() {
     const scene = new Scene(this.engine)
-    const camera = new ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 5, new Vector3(0, 0, 0))
+    const camera = new ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 15, new Vector3(0, 0, 0))
 
     camera.attachControl(this.renderCanvas, true)
 
     new HemisphericLight('light', new Vector3(1, 1, 0), scene)
 
     this.scene = scene
+
+    // 创建汽车通过纹理来渲染
     this.buildCar()
+
+    // 创建汽车通过模型来渲染
+    // SceneLoader.ImportMeshAsync('', 'https://assets.babylonjs.com/meshes/', 'car.glb').then(() => {
+    //   const car = scene.getMeshByName('car')
+
+    //   car.rotation = new BABYLON.Vector3(Math.PI / 2, 0, -Math.PI / 2)
+    //   car.position.y = 0.16
+    //   car.position.x = -3
+    //   car.position.z = 8
+
+    //   const animCar = new BABYLON.Animation(
+    //     'carAnimation',
+    //     'position.z',
+    //     30,
+    //     BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+    //     BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+    //   )
+
+    //   const carKeys = []
+
+    //   carKeys.push({
+    //     frame: 0,
+    //     value: 8,
+    //   })
+
+    //   carKeys.push({
+    //     frame: 150,
+    //     value: -7,
+    //   })
+
+    //   carKeys.push({
+    //     frame: 200,
+    //     value: -7,
+    //   })
+
+    //   animCar.setKeys(carKeys)
+
+    //   car.animations = []
+    //   car.animations.push(animCar)
+
+    //   scene.beginAnimation(car, 0, 200, true)
+
+    //   //wheel animation
+    //   const wheelRB = scene.getMeshByName('wheelRB')
+    //   const wheelRF = scene.getMeshByName('wheelRF')
+    //   const wheelLB = scene.getMeshByName('wheelLB')
+    //   const wheelLF = scene.getMeshByName('wheelLF')
+
+    //   scene.beginAnimation(wheelRB, 0, 30, true)
+    //   scene.beginAnimation(wheelRF, 0, 30, true)
+    //   scene.beginAnimation(wheelLB, 0, 30, true)
+    //   scene.beginAnimation(wheelLF, 0, 30, true)
+    // })
+
+    SceneLoader.ImportMeshAsync('', 'https://assets.babylonjs.com/meshes/', 'village.glb', scene)
 
     return scene
   }
