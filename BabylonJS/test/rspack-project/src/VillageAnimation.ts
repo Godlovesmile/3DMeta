577e7c8fc5f57c8de10f51cf45509756ae9e4287
lizhi <15501251520@163.com>
2023-04-10 15:14:16 +0800

    feat: dude && village

--- BabylonJS/test/rspack-project/src/VillageAnimation.ts
+++ BabylonJS/test/rspack-project/src/VillageAnimation.ts
@@ -16,6 +16,9 @@ import {
   Texture,
   Vector4,
   Animation,
+  Axis,
+  Tools,
+  Space,
 } from 'babylonjs'
 import 'babylonjs-loaders'
 
@@ -52,7 +55,7 @@ export default class VillageAnimation {
 
   createScene() {
     const scene = new Scene(this.engine)
-    const camera = new ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 50, new Vector3(0, 0, 0))
+    const camera = new ArcRotateCamera('camera', -Math.PI / 1.5, Math.PI / 2.2, 15, new Vector3(0, 0, 0))
 
     camera.attachControl(this.renderCanvas, true)
 
@@ -119,12 +122,64 @@ export default class VillageAnimation {
     // SceneLoader.ImportMeshAsync('', 'https://assets.babylonjs.com/meshes/', 'village.glb', scene)
 
     // 渲染人物模型
-    SceneLoader.ImportMeshAsync('', 'https://playground.babylonjs.com/scenes/Dude/', 'Dude.babylon', scene).then((result) => {
-      const dude = result.meshes[0]
-      dude.scaling = new Vector3(0.25, 0.25, 0.25)
+    // SceneLoader.ImportMeshAsync('', 'https://playground.babylonjs.com/scenes/Dude/', 'Dude.babylon', scene).then((result) => {
+    //   const dude = result.meshes[0]
+    //   dude.scaling = new Vector3(0.25, 0.25, 0.25)
 
-      scene.beginAnimation(result.skeletons[0], 0, 30, true, 1.0)
-    })
+    //   scene.beginAnimation(result.skeletons[0], 0, 30, true, 1.0)
+    // })
+
+    // 渲染任务在村庄中走动
+    const track = []
+    const walk = function(turn, dist) {
+      this.turn = turn
+      this.dist = dist
+    }
+
+    track.push(new walk(86, 7));
+    track.push(new walk(-85, 14.8));
+    track.push(new walk(-93, 16.5));
+    track.push(new walk(48, 25.5));
+    track.push(new walk(-112, 30.5));
+    track.push(new walk(-72, 33.2));
+    track.push(new walk(42, 37.5));
+    track.push(new walk(-98, 45.2));
+    track.push(new walk(0, 47))
+
+    SceneLoader.ImportMeshAsync('', 'https://assets.babylonjs.com/meshes/', 'village.glb', scene)
+    SceneLoader.ImportMeshAsync('him', 'https://playground.babylonjs.com/scenes/Dude/', 'Dude.babylon', scene).then(
+      (result) => {
+        const dude = result.meshes[0]
+
+        dude.scaling = new Vector3(0.008, 0.008, 0.008)
+        dude.position = new Vector3(-6, 0, 0.5)
+        dude.rotate(Axis.Y, Tools.ToRadians(-95), Space.LOCAL)
+
+        const startRotation = dude.rotationQuaternion.clone()
+
+        scene.beginAnimation(result.skeletons[0], 0, 100, true, 1.0)
+
+        let step = 0.015
+        let distance = 0
+        let p = 0
+        scene.onBeforeRenderObservable.add(() => {
+          dude.movePOV(0, 0, step)
+          distance += step
+
+          if (distance > track[p].dist) {
+            dude.rotate(Axis.Y, Tools.ToRadians(track[p].turn), Space.LOCAL)
+            p += 1
+            p %= track.length
+
+            if (p === 0) {
+              distance = 0
+              dude.position = new Vector3(-6, 0, 0)
+              dude.rotationQuaternion = startRotation.clone()
+            }
+          }
+        })
+      }
+    )
 
     return scene
   }
